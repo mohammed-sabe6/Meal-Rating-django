354b78027c888981c3cdad9948f814404f9a21eb
mohammedsabe <mohammedsabe6@gmail.com>
2022-09-21 00:24:28 +0300

    'vedio5'

--- mealrating/mealrating/wsgi.py
+++ mealrating/mealrating/wsgi.py
@@ -11,7 +11,7 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealrating.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mealrating3.settings')
 
 # application = get_wsgi_application()
 application = Cling(get_wsgi_application())
